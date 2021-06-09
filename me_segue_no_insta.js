6200b43d7ee9877a38363dd01d4dc70ad0390272
yasmin-calderon <yasmiin.allanny@gmail.com>
2021-06-08 22:11:33 -0300

    Chariizarda, vamos lá? prometo não cuspir fogo em você c;

---
+++ me_segue_no_insta.js
@@ -0,0 +1,11 @@
+function solucao(largura, altura) {
+    if (largura === altura) {
+        console.log("QUADRADA");
+    }
+    else if (largura > altura) {
+        console.log("PAISAGEM");
+    }
+    else {
+        console.log("RETRATO");
+    }
+}
